6ced3afa12c4a643903f7103b614972de089e109
rabuso <rabuso@edpnet.be>
2022-11-08 14:40:12 +0100

    Update App.js

--- part1/courseinfo/App.js
+++ part1/courseinfo/App.js
@@ -8,12 +8,22 @@ const Content = (props) =>
   <p key={key}>  {item.name} {item.exercises} </p>)
   
 )
+
+/* Calculate Total with reduce fn */
+
+const Total = (props) => {
+  let tot = props.total.reduce((sum, elem) => sum + elem.exercises , 0)
+return (<p>Number of exercises {tot}</p>)
+}
+
+/*
 const Total = (props) => (
 <p>Number of exercises {
  props.total[0].exercises +
  props.total[1].exercises + 
  props.total[2].exercises}</p>
 )
+*/
 
 const App = () => {
   const course = {
