50b8931d93fb248868216e30855de3bdf902e36f
rabuso <rabuso@edpnet.be>
2022-11-17 19:54:58 +0100

    Update App.js

--- part2/countries 2.12-2.14/src/App.js
+++ part2/countries 2.12-2.14/src/App.js
@@ -1,1 +1,32 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import React from 'react'
+import Filter from './components/Filter';
+import CountryList from './components/CountryList';
 
+const App = () => {  
+  const [countries, setCountries] = useState([])  
+  const [newFilter, setNewFilter] = useState("")
+  const [countriesToShow, setCountriesToShow] = useState([]);
+ 
+  useEffect(() => {    
+    axios.get("https://restcountries.com/v3.1/all").then( (response) => setCountries(response.data))  }, [])  
+  
+  const handleNewFilter = (event) =>  
+  { setNewFilter(event.target.value)
+    setCountriesToShow( countries.filter((c) => c.name.common.toLowerCase().includes(newFilter.toLowerCase())));  
+  }
+  
+  if (countriesToShow.length <= 10 && countriesToShow.length > 0)  {
+  return (
+    <div>
+      <Filter   searchOn={newFilter} handler={handleNewFilter}> </Filter>
+      <CountryList countries={countriesToShow} setCountry={setCountriesToShow}></CountryList>
+    </div> 
+  )}
+  else { 
+  return(<>
+      <Filter   searchOn={newFilter} handler={handleNewFilter}> </Filter>
+      <h2>Too many countries. Specify another filter</h2></>)}
+}
+export default App;
