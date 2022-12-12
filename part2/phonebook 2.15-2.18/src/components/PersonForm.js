bde5b947f171f39e335f4b0ded2ef7443d683443
rabuso <rabuso@edpnet.be>
2022-12-12 11:19:33 +0100

    Create PersonForm.js

---
+++ part2/phonebook 2.15-2.18/src/components/PersonForm.js
@@ -0,0 +1,16 @@
+const PersonForm = ({addPersons, newName, newNumber,handlePersonChange,handleNumberChange})  =>
+<form onSubmit={addPersons}>
+<div>
+  name: <input value={newName}
+               onChange={handlePersonChange}/>
+</div>
+<div>
+  number: <input value={newNumber}
+               onChange={handleNumberChange}/>
+</div>
+<div>
+  <button type="submit">add</button>
+</div>
+</form>
+
+export default PersonForm
